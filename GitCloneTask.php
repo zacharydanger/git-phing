6bb6366072dd10e894f036f301b9305fb195ed83
Zachary Danger <zacharydangercampbell@gmail.com>
2009-08-29 10:13:47 -0500

    Adding documentation.

--- GitCloneTask.php
+++ GitCloneTask.php
@@ -1,18 +1,32 @@
 <?php
 require_once 'GitTask.php';
 
-class GitCloneTask extends Task {
+/**
+ * Clones a git repository to a local directory.
+ *
+ * @author Zach Campbell <zacharydangercampbell@gmail.com>
+ */
+class GitCloneTask extends GitTask {
 	private $_repo;
 	private $_path;
 
+	/**
+	 * Sets the repository to clone.
+	 */
 	public function setRepo($repo) {
 		$this->_repo = $repo;
 	}
 
+	/**
+	 * Sets the target path for the cloned repository.
+	 */
 	public function setPath($path) {
 		$this->_path = $path;
 	}
 
+	/**
+	 * The main entry.
+	 */
 	public function main() {
 		if(false == isset($this->_repo) || false == isset($this->_path)) {
 			$this->log("GitCloneTask Fail: REPO and PATH must be set!\n");
