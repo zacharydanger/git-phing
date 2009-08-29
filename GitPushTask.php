f886a7f7c8961f3987fa814eb49577c431da72d0
Zachary Danger <zacharydangercampbell@gmail.com>
2009-08-29 10:22:18 -0500

    Adding more documentation.

--- GitPushTask.php
+++ GitPushTask.php
@@ -1,10 +1,18 @@
 <?php
 require_once 'GitTask.php';
 
+/**
+ * Pushes a git repo to some remote repo.
+ *
+ * @author Zach Campbell <zacharydangercampbell@gmail.com>
+ */
 class GitPushTask extends GitTask {
 	private $_remote;
 	private $_branch;
 
+	/**
+	 * Main entry point.
+	 */
 	public function main() {
 		$command = "git push " . $this->_remote . " " . $this->_branch;
 		$this->log("Pushing: " . $command);
@@ -12,6 +20,9 @@ class GitPushTask extends GitTask {
 		$this->log("Push Return: " . $return);
 	}
 
+	/**
+	 * Sets the remote repository to push to.
+	 */
 	public function setRemote($remote) {
 		$this->_remote = $remote;
 	}
