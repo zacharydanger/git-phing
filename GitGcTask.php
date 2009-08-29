f8b1b0acd3c5b82b6fdd5327ba60107b8ccc0d36
Zachary Danger <zacharydangercampbell@gmail.com>
2009-08-29 10:20:19 -0500

    Adding support for the defineable git path.

--- GitGcTask.php
+++ GitGcTask.php
@@ -3,7 +3,7 @@ require_once 'GitTask.php';
 
 class GitGcTask extends GitTask {
 	public function main() {
-		$command = "git gc";
+		$command = $this->git_path . " gc";
 		passthru($command, $return);
 		$this->log("Cleaing up git repo: " . $return);
 	}
