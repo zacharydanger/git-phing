dc38e8b56666b87cdaf2164e4dd8db30aad40505
Zachary Danger <zacharydangercampbell@gmail.com>
2009-08-29 10:04:57 -0500

    Updating the base GitTask to allow for optional setting of the git path.

--- GitTask.php
+++ GitTask.php
@@ -2,8 +2,18 @@
 require_once 'phing/Task.php';
 
 class GitTask extends Task {
-	public function init() {
-		$this->log("TODO: Make sure we're in a git repo.");
+	/**
+	 * Path to git executable.
+	 */
+	public $git_path = 'git';
+
+	public function init() { /* do nothing */ }
+
+	/**
+	 * Optionally set the path to git.
+	 */
+	public function setGitPath($git_path) {
+		$this->git_path = $git_path;
 	}
 
 	public function main() { /* do nothing */ }
