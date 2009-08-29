dbeda11dc3c6e9a218fe8521e4f82b9e71796118
Zachary Danger <zacharydangercampbell@gmail.com>
2009-08-29 10:24:20 -0500

    Adding documentation.

--- GitSubmoduleUpdateTask.php
+++ GitSubmoduleUpdateTask.php
@@ -1,18 +1,32 @@
 <?php
 require_once 'GitTask.php';
 
+/**
+ * Initializes and updates git submodules.
+ *
+ * @author Zach Campbell <zacharydangercampbell@gmail.com>
+ */
 class GitSubmoduleUpdateTask extends GitTask {
+	/**
+	 * Main entry point.
+	 */
 	public function main() {
 		$this->_initSubmodules();
 		$this->_updateSubmodules();
 	}
 
+	/**
+	 * Initializes submodules.
+	 */
 	private function _initSubmodules() {
 		$command = "git submodule foreach git submodule init";
 		passthru($command, $return);
 		$this->log("Initializing Git Submodules: " . $return);
 	}
 
+	/**
+	 * Updates submodules.
+	 */
 	private function _updateSubmodules() {
 		$command = "git submodule foreach git submodule update";
 		passthru($command, $return);
