d603bc138e1081356261d6c3d463e85dfd4dc313
Zachary Danger <zacharydangercampbell@gmail.com>
2009-08-29 10:26:31 -0500

    Adding docs.

--- GitTagTask.php
+++ GitTagTask.php
@@ -1,13 +1,24 @@
 <?php
 require_once 'GitTask.php';
 
+/**
+ * Creates a new tag in git.
+ *
+ * @author Zach Campbell <zacharydangercampbell@gmail.com>
+ */
 class GitTagTask extends GitTask {
 	private $_tag_name;
 
+	/**
+	 * Sets the name for the new git tag.
+	 */
 	public function setTag($tag_name) {
 		$this->_tag_name = $tag_name;
 	}
 
+	/**
+	 * Main entry point.
+	 */
 	public function main() {
 		$cmd = "git tag " . $this->_tag_name;
 		$this->log("Running " . $cmd);
