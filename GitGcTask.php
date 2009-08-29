89e8c4f092c6b63215487171f4d820063f77a98e
Zachary Danger <zacharydangercampbell@gmail.com>
2009-08-29 10:20:43 -0500

    Adding documentation.

--- GitGcTask.php
+++ GitGcTask.php
@@ -1,7 +1,15 @@
 <?php
 require_once 'GitTask.php';
 
+/**
+ * Runs `git gc` in the local dir.
+ *
+ * @author Zach Campbell <zacharydangercampbell@gmail.com>
+ */
 class GitGcTask extends GitTask {
+	/**
+	 * Main entry.
+	 */
 	public function main() {
 		$command = $this->git_path . " gc";
 		passthru($command, $return);
