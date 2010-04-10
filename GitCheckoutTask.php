72ecddb5c3efdf7bdf90a25e945f349ffff54abf
Beau D. Simensen <beau@dflydev.com>
2010-04-10 16:24:21 +0800

    o Updates to the clone task, added a checkout task.

---
+++ GitCheckoutTask.php
@@ -0,0 +1,94 @@
+<?php
+require_once 'GitTask.php';
+
+/**
+ * Perform a checkout in git.
+ * @author Beau Simensen <simensen@gmail.com>
+ */
+class GitCheckoutTask extends GitTask {
+
+	/**
+	 * Tag name to checkout
+	 * @var string
+	 */
+	private $_tag_name = null;
+
+	/**
+	 * Branch name to checkout
+	 * @var string
+	 */
+	private $_branch_name = null;
+
+	/**
+	 * Path to cloned repository
+	 * @var string
+	 */
+	private $_path = null;
+
+	/**
+	 * Sets the name for the git tag.
+	 */
+	public function setTag($tag_name) {
+		$this->_tag_name = $tag_name;
+	}
+
+	/**
+	 * Sets the name for the git branch.
+	 */
+	public function setBranch($branch_name) {
+		$this->_branch_name = $branch_name;
+	}
+
+	/**
+	 * Sets the target path for the cloned repository.
+	 */
+	public function setPath($path) {
+		$this->_path = $path;
+	}
+
+	/**
+	 * Main entry point.
+	 */
+	public function main() {
+
+		if(false == isset($this->_path)) {
+			$this->log("GitCheckoutTask Fail: PATH must be set!\n");
+			exit(1);
+		}
+
+		if(false == isset($this->_tag_name) and false == isset($this->_branch_name)) {
+			$this->log("GitCheckoutTask Fail: TAG or BRANCH must be set!\n");
+			exit(1);
+		}
+
+		$which = null;
+
+		$dir = getcwd();
+		chdir($this->_path);
+
+		if ( $this->_tag_name !== null ) {
+			$which = $this->_tag_name;
+		}
+
+		if ( $this->_branch_name !== null ) {
+			$which = 'origin/' . $this->_branch_name;
+		}
+
+		$cmd = $this->git_path . ' checkout ' . $which;
+
+		$this->log("Running " . $cmd);
+		passthru($cmd, $return);
+		$this->log("Return: " . $return);
+
+		chdir($dir);
+
+		if(intval($return) > 0) {
+			if ( intval)
+			$this->log("Git Checkout Failed.");
+			exit(1);
+		}
+
+	}
+
+}
+?>
--- GitCloneTask.php
+++ GitCloneTask.php
@@ -32,12 +32,23 @@ class GitCloneTask extends GitTask {
 			$this->log("GitCloneTask Fail: REPO and PATH must be set!\n");
 			exit(1);
 		}
-		$command = $this->git_path . " clone " . $this->_repo . " " . $this->_path;
-		$this->log("Attempting to clone '" . $this->_repo . "' into '" . $this->_path . "'");
-		passthru($command, $return);
-		if(intval($return) > 0) {
-			$this->log("Git Clone Failed.");
-			exit(1);
+		if ( file_exists($this->_path) ) {
+			// Check to see if the path already exists. If it does, try to
+			// do a fetch.
+			$dir = getcwd();
+			chdir($this->_path);
+			$command = $this->git_path . " fetch";
+			passthru($command, $return);
+			chdir($dir);
+		} else {
+			$command = $this->git_path . " clone " . $this->_repo . " " . $this->_path;
+			$this->log("Attempting to clone '" . $this->_repo . "' into '" . $this->_path . "'");
+			$this->log("Running " . $command);
+			passthru($command, $return);
+			if(intval($return) > 0) {
+				$this->log("Git Clone Failed.");
+				exit(1);
+			}
 		}
 	}
 }
